e7884b93edc645a96d7882a4a01809a30c768cea
marriamdd <davitashvili.m3@gmail.com>
2024-03-27 17:20:50 +0400

    add form for countside component with some  syles

--- src/Components/CountSide.jsx
+++ src/Components/CountSide.jsx
@@ -1,7 +1,21 @@
-
-
+import { Label } from "../Ui/Title";
+import { Input } from "../Ui/Input";
+import styled from "styled-components";
 export default function CountSide() {
   return (
-    <div>CountSide</div>
-  )
-} 
+    <div>
+      <Form>
+        <Label >Bill</Label>
+        <Input type="text" />
+
+      </Form>
+    </div>
+  );
+}
+
+const Form=styled.form`
+  display: flex;
+  flex-direction: column;
+  padding: 6.2rem 3.2rem ;
+
+`
